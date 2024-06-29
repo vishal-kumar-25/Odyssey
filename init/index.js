e2048034662188671d85ebb03d24f2171cf4b5d5
Vishal Kumar <vishal.kumar.gkdu@gmail.com>
2024-06-29 17:57:28 +0530

    Updating index.js

--- init/index.js
+++ init/index.js
@@ -1,29 +1,29 @@
-const mongoose = require("mongoose");
-const initData = require("./data.js");
-const Listing = require("../models/listing.js");
+// const mongoose = require("mongoose");
+// const initData = require("./data.js");
+// const Listing = require("../models/listing.js");
 
-const MONGO_URL = process.env.ATLASDB_URL;
+// const MONGO_URL = process.env.ATLASDB_URL;
 
-main()
-.then(() => {
-    console.log("connected to DB");
-})
-.catch((err) => {
-    console.log(err);
-});
+// main()
+// .then(() => {
+//     console.log("connected to DB");
+// })
+// .catch((err) => {
+//     console.log(err);
+// });
 
-async function main() {
-    await mongoose.connect(MONGO_URL);
-}
+// async function main() {
+//     await mongoose.connect(MONGO_URL);
+// }
 
-const initDB = async () => {
-    await Listing.deleteMany({});
-   initData.data = initData.data.map((obj) => ({...obj, owner: "66657747d75daba8fdf6b8f8"}));
-    await Listing.insertMany(initData.data);
-    console.log("data was initialized");
-};
+// const initDB = async () => {
+//     await Listing.deleteMany({});
+//    initData.data = initData.data.map((obj) => ({...obj, owner: "66657747d75daba8fdf6b8f8"}));
+//     await Listing.insertMany(initData.data);
+//     console.log("data was initialized");
+// };
 
-initDB();
+// initDB();
 
 
 
