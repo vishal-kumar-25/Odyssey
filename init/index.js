76a51fdd814b82124b4728a74c05fa757d4f4cc3
Vishal Kumar <vishal.kumar.gkdu@gmail.com>
2024-06-30 12:39:56 +0530

    Updating index.js

--- init/index.js
+++ init/index.js
@@ -2,23 +2,27 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = process.env.ATLASDB_URL;
+//const MONGO_URL = process.env.ATLASDB_URL;
+const dbUrl = process.env.ATLASDB_URL;
+
 
 main()
-  .then(() => {
-    console.log("Connected to DB");
-    return initDB(); // Initialize the DB after connecting
-  })
-  .catch((err) => {
-    console.error("Error connecting to DB:", err);
-  });
+.then(() => {
+    console.log("connected to DB");
+})
+.catch((err) => {
+    console.log(err);
+});
 
 async function main() {
-  await mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
-  await Listing.deleteMany({});
-  await Listing.insertMany(initData.data);
-  console.log("Data was initialized");
+    await Listing.deleteMany({});
+   initData.data = initData.data.map((obj) => ({...obj, owner: "66657747d75daba8fdf6b8f8"}));
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
 };
+
+initDB();
